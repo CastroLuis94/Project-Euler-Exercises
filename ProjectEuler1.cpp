4e3c40993d54ab95ce01549fce284a15e5376ec6
Luiscastro1994 <luisitocastro94@hotmail.com>
2017-03-16 17:05:06 -0300

    Update ProjectEuler1.cpp

--- ProjectEuler1.cpp
+++ ProjectEuler1.cpp
@@ -1,1 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int MultiplosHasta(int n,int limite){
+	int i = 1;
+	int res = 0;
+	while(n*i < limite){
+		res += n*i;
+		i++;
+	}
+	return res;
+}
+
+int main (int argc, char *argv[]) {
+	
+	int multiplosDeTres = MultiplosHasta(3,1000);
+	int multiplosDeCinco = MultiplosHasta(5,1000);
+	int multiplosDeQuince = MultiplosHasta(15,1000);
+	int res = multiplosDeTres+multiplosDeCinco-multiplosDeQuince;
+	cout << res <<endl;
+	return 0;
+}
 
